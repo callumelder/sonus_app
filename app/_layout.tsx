d01c2b495cdc4c26c02898af6fa3f0e1d16c749d
Callum Elder <cjames.02@icloud.com>
2025-06-14 20:13:55 +1000

    revert layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,5 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+  return <Stack />;
 }
